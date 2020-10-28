86a57c9791e4241494768006ce1210cb835600f9
amar <amarbugarin@outlook.com>
2020-10-28 19:45:56 +0100

    initial commit-m

---
+++ gui.py
@@ -0,0 +1,32 @@
+import index
+import tkinter
+
+root = tkinter.Tk()
+canvas1 = tkinter.Canvas(root, width=400, height=300)
+heading = tkinter.Text(root, width=400, height=20)
+canvas1.pack()
+
+entry1 = tkinter.Entry(root)
+entry2 = tkinter.Entry(root)
+
+canvas1.create_window(200, 140, window=entry1)
+canvas1.create_window(200, 30, window=entry2)
+
+
+def get_input_data():
+    # direction = entry1.get()
+    x = index.getUrl(entry1.get())
+
+    if index.getUrl(entry1.get()):
+        label1 = tkinter.Label(root, text="Radi uspjesno ste poslali")
+        canvas1.create_window(200, 70, window=label1)
+        index.scrapping_data(entry2.get(), x)
+    else:
+        label2 = tkinter.Label(root, text="Pogresno ste unijeli parametre")
+        canvas1.create_window(200, 70, window=label2)
+
+
+button = tkinter.Button(text="Skrejpaj stranicu", command=get_input_data)
+canvas1.create_window(200, 250, window=button)
+
+root.mainloop()
